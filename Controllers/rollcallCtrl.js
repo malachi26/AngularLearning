095bb0144b7d9c815c0f3aa91de8d52671547fde
Lyle Hart III <lyle.hart@cellularsales.com>
2016-03-23 10:33:47 -0400

    commit all work at work

---
+++ Controllers/rollcallCtrl.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('rollcallCtrl', rollcallCtrl);
+
+    rollcallCtrl.$inject = ['$location','$http']; 
+
+    function rollcallCtrl($location,$http) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'rollcallCtrl';
+        
+        var getStudents = $http.get("data\studentData.txt")
+            .success(function (response) {
+                vm.students = response;
+            });
+
+    }
+})();
